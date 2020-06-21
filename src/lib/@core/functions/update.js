5fc3648d58f336858ee70b40da859576519e0c40
aacgn <4augusto@gmail.com>
2020-06-21 14:54:04 -0300

    Atomic
    - Hot Fix: Update was passing the new data as a Array

--- src/lib/@core/functions/update.js
+++ src/lib/@core/functions/update.js
@@ -4,7 +4,7 @@ export function updateContext(ref, contextAttr, contextVal) {
     // Deep copy
     const initialContext = JSON.parse(JSON.stringify(ref.props.context));
 
-    ref.props.context[contextAttr] = [contextVal];
+    ref.props.context[contextAttr] = contextVal;
 
     if (initialContext !== ref.props.context) {
         unmountContent(ref.props.mount(), ref.dom);
